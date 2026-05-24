54fff43638e33d17da39ff28d22bc5252c441001
Tyler Kinney <kinneytyler101@gmail.com>
2026-05-23 22:50:18 -0700

    Checkpoint 1

---
+++ R/load_data.R
@@ -0,0 +1,13 @@
+#' Load Break Free From Plastic Data
+#'
+#' @return a data frame
+#'
+#' @importFrom tidytuesdayR tt_load
+#' @export
+
+load_data <- function() {
+  tuesdata <- tt_load('2021-01-26')
+  plastics <- tuesdata$plastics
+
+  return(plastics)
+}
