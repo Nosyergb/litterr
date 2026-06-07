99a631cfafb0cec0bc2fc4f844350ffd7fc50d35
Nosyergb <grballes@calpoly.edu>
2026-06-06 22:06:14 -0700

    more updates

--- R/load_data.R
+++ R/load_data.R
@@ -1,13 +1,13 @@
 #' Load Break Free From Plastic Data
 #'
 #' @return a data frame
-#'
-#' @importFrom tidytuesdayR tt_load
 #' @export
-
 load_data <- function() {
-  tuesdata <- tt_load('2021-01-26')
-  plastics <- tuesdata$plastics
 
-  return(plastics)
+  file_path <- system.file(
+    "plastics.rds",
+    package = "projectr"
+  )
+
+  readRDS(file_path)
 }
